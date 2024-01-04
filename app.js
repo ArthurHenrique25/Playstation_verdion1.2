b0aa328ee3957c9cb245d067f90b313fc8e5edaa
Arthur Nogueira <arthur.nogueira@GMVB.LOCAL>
2024-01-04 17:42:47 -0300

    fsdsadsadsad

--- app.js
+++ app.js
@@ -1,37 +1,19 @@
-// function botao(){
-//     alert('clicou')
-//     document.querySelector('h1').style.cursor = '';
-// }
-function atualizarRelogio() {
-    var agora = new Date();
-    var hora = agora.getHours();
-    var minutos = agora.getMinutes();
-
-    hora = hora < 10 ? '0' + hora : hora;
-    minutos = minutos < 10 ? '0' + minutos : minutos;
-
-    var horaAtual = hora + ':' + minutos;
-
-    document.getElementById('relogio').innerHTML = horaAtual+ " PM";
-
-    setTimeout(atualizarRelogio, 1000 * 60); // Atualizar a cada minuto
-}
-atualizarRelogio();
-
-function mostrarImagem(caminhoImagem) {
-    var telaImagem = document.getElementById('telaImagem');
-    var imagemAmpliada = document.getElementById('imagemAmpliada');
-    
-    imagemAmpliada.src = caminhoImagem;
-    telaImagem.style.display = 'flex';
+function pesquisar() {
+    var overlay = document.getElementById("overlay");
+    overlay.style.display = "flex";
 }
 
-function ocultarImagem() {
-    var telaImagem = document.getElementById('telaImagem');
-    telaImagem.style.display = 'none';
+function fecharPesquisa() {
+    var overlay = document.getElementById("overlay");
+    overlay.style.display = "none";
 }
 
-function acaoJogar() {
-    // Adicione a lógica de jogo ou ação desejada aqui
-    
+function config(){
+    var overlay = document.getElementById("configuracao_overlay");
+    overlay.style.display = "flex";
+
+}
+function fecharConfig() {
+    var overlay = document.getElementById("configuracao_overlay");
+    overlay.style.display = "none";
 }
